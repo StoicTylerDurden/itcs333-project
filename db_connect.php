680ce3c350192ac08ecce5109edb0dce16ad17e2
Jaber <buhussayyen@gmail.com>
2024-11-23 23:40:58 +0300

    connection to local database & database schema (room_booking.sql)

--- db_connect.php
+++ db_connect.php
@@ -1,22 +1,13 @@
 <?php
-
-// Make sure you create a config file I've shared it via whatsapp
-// Then create a file named ".gitignore" in this file include "config.php"
-
-// include the config file which has the credentials
-require_once 'config.php';
-
-$dsn = "mysql:host=" . DATABASE_HOSTNAME . ";dbname=" . DATABASE_NAME;
-$username = DATABASE_USERNAME;
-$password = DATABASE_PASSWORD;
-
+$dsn = "mysql: host=localhost;dbname=room_booking";
+$username = "root";
+$password = "";
 try {
-    // Create a new PDO instance
-    $pdo = new PDO($dsn, $username, $password);
-    echo "Connected successfully to the database!";
-} catch (PDOException $e) {
+    $db = new PDO($dsn, $username, $password);
+    echo " You have connected to room_booking database";
+}catch(PDOException $e) {
     $error_message = $e->getMessage();
     echo $error_message;
     exit();
-}
+} 
 ?>
