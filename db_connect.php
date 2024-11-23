fb458dba9e0ab94fd9270ef936f44d93eb9ea28c
Jaber <buhussayyen@gmail.com>
2024-11-23 23:58:19 +0300

    connection to db file

---
+++ db_connect.php
@@ -0,0 +1,15 @@
+<?php
+$dsn = "mysql: host=localhost;dbname=room_booking";
+$username = "root";
+$password = "";
+try {
+    
+    // Create a new PDO instance
+    $db = new PDO($dsn, $username, $password);
+    echo " You have connected to room_booking database";
+} catch (PDOException $e) {
+    $error_message = $e->getMessage();
+    echo $error_message." Cannot connect to the database!!\nTry again";
+    exit();
+} 
+?>
