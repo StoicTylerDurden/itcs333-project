e2493c85518f7eed412f75cf1fe988012f8e8065
Mohdalmajed <mmealmajed@gmail.com>
2024-12-02 18:53:47 +0300

    Custom admin navbar

---
+++ navbar_admin.php
@@ -0,0 +1,24 @@
+<!-- I took this from bootstrap -->
+<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark fixed-top">
+  <a class="navbar-brand" href="#">Room Booking</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="admin_panel.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="View_room_fazil.php">Schedule Managment <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="account.php">Account</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="logout.php">logout</a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
