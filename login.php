6ba19e3cfc26870277a076bb0371d835895aeb48
Abdulrahman <dahoomgamer22@gmail.com>
2024-12-02 14:42:09 +0300

    Added login/signup style file /Login.css + Modified Login and signup page (changed submit input to button + added classes and propper html tags)

--- login.php
+++ login.php
@@ -7,28 +7,35 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
-    <h2>Login</h2>
-    <?php
-    if (isset($_SESSION['login_error'])) {
-        echo "<p style='color: red;'>" . $_SESSION['login_error'] . "</p>";
-        unset($_SESSION['login_error']);
-    }
-    if (isset($_SESSION['registration_success'])) {
-        echo "<p style='color: green;'>" . $_SESSION['registration_success'] . "</p>";
-        unset($_SESSION['registration_success']);
-    }
-    ?>
-    <form action="login_process.php" method="post">
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br><br>
+    <main>
+        <div class="con">
+            <h1>Login</h1>
+            <div class="login-wrapper">
+                <?php
+                if (isset($_SESSION['login_error'])) {
+                    echo "<p style='color: red;'>" . $_SESSION['login_error'] . "</p>";
+                    unset($_SESSION['login_error']);
+                }
+                if (isset($_SESSION['registration_success'])) {
+                    echo "<p style='color: green;'>" . $_SESSION['registration_success'] . "</p>";
+                    unset($_SESSION['registration_success']);
+                }
+                ?>
+                <form action="login_process.php" method="post">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required>
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br><br>
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" required>
 
-        <input type="submit" value="Login">
-    </form>
-    <p>Don't have an account? <a href="register.php">Register here</a></p>
+                    <button type="submit">Login</button>
+                </form>
+                <p>Don't have an account? <a href="register.php">Register here</a></p>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
--- register.php
+++ register.php
@@ -55,27 +55,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
-    <h2>Register</h2>
-    <?php
-    if (isset($_SESSION['registration_error'])) {
-        echo "<p style='color: red;'>" . $_SESSION['registration_error'] . "</p>";
-        unset($_SESSION['registration_error']);
-    }
-    ?>
-    <form action="register.php" method="post">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required><br><br>
+    <main>
+        <div class="con">
+            <h1>Register</h1>
+            <div class="login-wrapper">
+                <?php
+                if (isset($_SESSION['registration_error'])) {
+                    echo "<p style='color: red;'>" . $_SESSION['registration_error'] . "</p>";
+                    unset($_SESSION['registration_error']);
+                }
+                ?>
+                <form action="register.php" method="post">
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" name="name" required>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br><br>
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required>
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br><br>
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" required>
 
-        <input type="submit" value="Register">
-    </form>
-    <p>Already have an account? <a href="login.php">Login here</a></p>
+                    <button type="submit">Register</button>
+                </form>
+                <p>Already have an account? <a href="login.php">Login here</a></p>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
