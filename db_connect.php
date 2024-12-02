0a31d736885b0572f3efe1957d28e9beeda9598a
programmerfaz <fazilmohd456@gmail.com>
2024-12-02 09:18:51 +0300

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -8,8 +8,6 @@ try {
     $pdo = new PDO($dsn, $username, $password);
     // echo " You have connected to room_booking database";
     
-    // Enable error reporting
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 
 } catch (PDOException $e) {
     $error_message = $e->getMessage();
